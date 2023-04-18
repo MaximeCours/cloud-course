d7e8c25dd8c9c867e5395c0957f6aa3bc5534a05
Raphaël Oester <raphaeloester@gmail.com>
2023-04-18 15:53:36 +0300

    added routing resources

--- networking.tf
+++ networking.tf
@@ -12,3 +12,26 @@ resource aws_subnet "public" {
     Name = "2im-Demo-Public"
   }
 }
+
+resource aws_internet_gateway "default" {
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "2im-Demo"
+  }
+}
+
+resource aws_route_table "public" {
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+  tags = {
+    Name = "2im-Demo-Public"
+  }
+}
+
+resource aws_route_table_association "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
