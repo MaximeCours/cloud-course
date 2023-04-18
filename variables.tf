338fe520dff80dd8bc5a21bf67e9d635be1695a0
Raphaël Oester <raphaeloester@gmail.com>
2023-04-18 13:30:57 +0300

    fixed quotes around variable types, first apply

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable AWS_REGION {
-    type = "string"
+    type = string
     default = "eu-north-1"
 }
 
 variable AWS_ACCESS_KEY {
-  type = "string"
+  type = string
 }
 
 variable AWS_SECRET_KEY {
-  type = "string"
+  type = string
 }
